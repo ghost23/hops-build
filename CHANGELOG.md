25227b8bc3ccd39361089366205feeb8c5e252dc
Sven Busse <mail@ghost23.de>
2017-11-07 17:44:12 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,47 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+<a name="7.4.0"></a>
+# [7.4.0](https://github.com/xing/hops/compare/v7.3.5...v7.4.0) (2017-11-01)
+
+
+
+
+**Note:** Version bump only for package hops-build
+
+<a name="7.3.0"></a>
+# [7.3.0](https://github.com/xing/hops/compare/v7.2.0...v7.3.0) (2017-10-26)
+
+
+### Features
+
+* **build:** add standalone commands compatible with local-cli ([efc7c53](https://github.com/xing/hops/commit/efc7c53))
+
+
+
+
+<a name="7.2.0"></a>
+# [7.2.0](https://github.com/xing/hops/compare/v7.1.0...v7.2.0) (2017-10-18)
+
+
+
+
+**Note:** Version bump only for package hops-build
+
+<a name="7.1.0"></a>
+# [7.1.0](https://github.com/xing/hops/compare/v7.0.0...v7.1.0) (2017-10-16)
+
+
+
+
+**Note:** Version bump only for package hops-build
+
+<a name="7.0.0"></a>
+# [7.0.0](https://github.com/xing/hops/compare/v6.2.8...v7.0.0) (2017-10-13)
+
+
+### Features
+
+* **build:** split hops packages, introduce changelog
---
+++ build.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+
+var hopsConfig = require('hops-config');
+var hopsBuildConfig = require('hops-build-config');
+var HopsPlugin = require('hops-plugin');
+
+var buildConfig = require(hopsBuildConfig.buildConfig);
+var nodeConfig = require(hopsBuildConfig.nodeConfig);
+
+var cleanup = require('./lib/cleanup');
+
+var mergeWithPlugins = merge.strategy({ plugins: 'append' });
+
+function getWebpackConfig (options) {
+  if (options.static) {
+    return mergeWithPlugins(
+      buildConfig,
+      {
+        plugins: [
+          new HopsPlugin(
+            hopsConfig.locations,
+            nodeConfig
+          ),
+          new webpack.ProgressPlugin()
+        ]
+      }
+    );
+  } else {
+    return [buildConfig, nodeConfig].map(function (config) {
+      return mergeWithPlugins(
+        config,
+        {
+          plugins: [
+            new webpack.ProgressPlugin()
+          ]
+        }
+      );
+    });
+  }
+}
+
+function defaultCallback (error, stats) {
+  if (error) {
+    console.error(error.stack.toString());
+  } else {
+    console.log(stats.toString({ chunks: false }));
+  }
+}
+
+module.exports = function runBuild (options, callback) {
+  function build () {
+    return webpack(getWebpackConfig(options)).run(callback || defaultCallback);
+  }
+
+  if (options.clean) {
+    var dirs = [hopsConfig.buildDir, hopsConfig.cacheDir];
+    return cleanup(dirs).then(build);
+  } else {
+    return build();
+  }
+};
